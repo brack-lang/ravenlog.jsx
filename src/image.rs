01441fcb468f24c2abeb3c5bcd7df1225152433c
Mutsuha Asada <me@momee.mt>
2025-03-14 12:13:17 +0900

    add: alt and caption to image.rs

--- src/image.rs
+++ src/image.rs
@@ -5,16 +5,16 @@ pub(crate) fn metadata_image() -> Metadata {
     Metadata {
         command_name: "img".to_string(),
         call_name: "image".to_string(),
-        argument_types: vec![("src".to_string(), Type::TInline)],
+        argument_types: vec![("src".to_string(), Type::TInline), ("alt".to_string(), Type::TInline), ("caption".to_string(), Type::TOption(Box::new(Type::TInline)))],
         return_type: Type::TBlock,
     }
 }
 
 #[plugin_fn]
 pub fn image(Json(args): Json<Vec<Value>>) -> FnResult<String> {
-    if args.len() != 1 {
+    if args.len() != 2 && args.len() != 3 {
         return Err(WithReturnCode::new(
-            anyhow::anyhow!("Usage: {{ravenlog.img src}}"),
+            anyhow::anyhow!("Usage: {{ravenlog.img src, alt [, caption]}}"),
             1,
         ));
     }
@@ -27,5 +27,27 @@ pub fn image(Json(args): Json<Vec<Value>>) -> FnResult<String> {
             ))
         }
     };
-    Ok(format!("<RavenlogImage src=\"{}\"></RavenlogImage>", src))
+    let alt = match &args[1] {
+        Value::Text(t) => t,
+        _ => {
+            return Err(WithReturnCode::new(
+                anyhow::anyhow!("alt must be Value::Text"),
+                1,
+            ))
+        }
+    };
+    let caption = match args.get(2) {
+        Some(Value::Text(t)) => Some(t),
+        None => None,
+        _ => {
+            return Err(WithReturnCode::new(
+                anyhow::anyhow!("caption must be Value::Text"),
+                1,
+            ))
+        }
+    };
+    match caption {
+        Some(caption) => Ok(format!("<RavenlogImage src=\"{}\" alt=\"{}\" caption=\"{}\"></RavenlogImage>", src, alt, caption)),
+        None => Ok(format!("<RavenlogImage src=\"{}\" alt=\"{}\"></RavenlogImage>", src, alt)),
+    }
 }
